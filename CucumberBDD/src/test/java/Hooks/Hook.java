83a836e71b27e78a3742487f09b50304fadd468f
wprjavanguser <wprjavanguser@WPRJAVANG.w1lf0opmxjoevagrkbjstac3gd.rx.internal.cloudapp.net>
2026-06-05 19:11:07 +0530

    new commit

---
+++ CucumberBDD/src/test/java/Hooks/Hook.java
@@ -0,0 +1,38 @@
+package Hooks;
+
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Hook {
+
+    public static WebDriver driver;
+
+    public void setup() {
+
+       /* ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+
+        driver = new ChromeDriver(options);*/
+    	driver=new EdgeDriver();
+
+        driver.manage().timeouts()
+              .implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://tutorialsninja.com/demo/");
+    }
+
+    public void closes() {
+        if(driver != null) {
+            driver.quit();
+        }
+    }
+}
